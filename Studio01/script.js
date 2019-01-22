65198ffcc0db438d9e728eb6c98d0ebd9d22311f
Chad Fry <chadefry@gmail.com>
2019-01-22 11:16:49 -0600

    Added First Project.
    The first project has been added to the WebDevStudios folder. it is up-to-date and needs no changes unless specified by the professor.

---
+++ Studio01/script.js
@@ -0,0 +1,56 @@
+/*jslint browser: true, indent: 3 */
+
+// CS 3312, spring 2019
+// Studio 1
+// YOUR NAMES: Joshua Meek, Chad Fry
+
+// All the code below will be run once the page content finishes loading.
+document.addEventListener('DOMContentLoaded', function () {
+
+   // Enforce stricter JavaScript rules.
+   'use strict';
+
+   // Declare this function's local variables.
+   var howdyElement, nameInputElement, nameOutputElement, submitNameButton;
+
+   //Declare "webAppIdeaElement" to use with new event listener below
+   var webAppIdeaElement;
+
+   // Find all needed elements and save them in variables.
+   nameInputElement = document.querySelector('#name-input');
+   submitNameButton = document.querySelector('#submit-name');
+   howdyElement = document.querySelector('#howdy');
+   nameOutputElement = document.querySelector('#name-output');
+   webAppIdeaElement = document.querySelector('#web-app-idea');
+
+   // Make things happen when a user clicks on the button element.
+   submitNameButton.addEventListener('click', function () {
+      var name;
+
+      // Get the string value out of the input textbox.
+      name = nameInputElement.value;
+
+      if (name.length === 0) {
+         // The user didn't input a name, so use a default.
+         nameOutputElement.textContent = 'student';
+         howdyElement.classList.remove('enthusiastic');
+      } else {
+         // The user did input a name, so use it.
+         nameOutputElement.textContent = name;
+         // Make the paragraph stand out more.
+         howdyElement.classList.add('enthusiastic');
+      }
+   }, false);
+
+   // ADD NEW EVENT LISTENER HERE
+
+   //Event listener for "webAppIdeaElement" that will copy text from paragraph element
+   //into the id "name-input" when clicked
+   webAppIdeaElement.addEventListener('click', function () {
+      var paragraphContents;
+      paragraphContents = webAppIdeaElement.textContent;
+      nameInputElement.value = paragraphContents;
+      webAppIdeaElement.classList.toggle('clicked');
+   }, false);
+
+}, false);
