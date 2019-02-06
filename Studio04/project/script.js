4353aef3d1b3a9d21485225ec0f0e5f235d12191
spiffylich <Blackpapermoon42@gmail.com>
2019-02-06 11:15:48 -0600

    Finished work on temperature box.
    the F to C conversion box is now pretty much finished.

--- Studio04/project/script.js
+++ Studio04/project/script.js
@@ -4,46 +4,55 @@
 // Studio 4
 // YOUR NAMES: Chad Fry, Josh Meek
 
+
 // All the code below will be run once the page content finishes loading.
 document.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
-  /* Example code given in-class!
-
-   //Declare this function's local vars.
-  var outputElement, num, whatever;
-
-  // Save the output box in a var.
-  outputElement = document.querySelector('#output'); //we can use querySelector to get ANYTHING :O
-
-  //Empty the text in the output box.
-  outputElement.value = '';
-
-  // Calculate a number and save it in a variable
-  num = 0.1 + 0.2;
-
-  outputElement.value += num +  '\n';
 
 
-  num = Number(outputElement.value);
-  if(typeof num === 'number')
-  {
-      outputElement.value += num + ' is a string' + '\n';
-      outputElement.value += num.toFixed(6) +  '\n';
-  }
-
-
-*/
-
-
-  //Actual code goes down below.
+  // *** The user has clicked "convert fahrenheit to celsius" ***
   document.querySelector('#convert-f-to-c').addEventListener('click', function() {
     var fahrenheitVal, celsiusVal;
 
-    //grab the value inside the fahrenheit box, but convert it to an int (by default it may be string).
-    fahrenheitVal = parseInt(document.querySelector('#fahrenheit').value, 10);
-    //test outputting before we get to any serious work.
-    document.querySelector('#celsius').value = 'the value of fahrenheit is: ' + fahrenheitVal;
-  }, false); //end of
+    // grab the value inside the fahrenheit box, but convert it to a float (by default it may be string).
+    fahrenheitVal = parseFloat(document.querySelector('#fahrenheit').value, 10);
+
+    // calculate the correct value for celsius, to 3 decimal places. formula was online.
+    celsiusVal = ((fahrenheitVal - 32) * (5.0 / 9.0)).toFixed(3);
+
+    // catch cases where the user gave something that wasn't a number
+    if (isNaN(celsiusVal)) {
+      document.querySelector('#celsius').value = 'Bad input.';
+    } else {
+      document.querySelector('#celsius').value = celsiusVal; //only convert valid nums.
+    }
+
+  }, false); //end of "convert fahrenheit to celsius"
+
+
+
+  // *** The user has clicked "convert celsius to fahrenheit" ***
+  document.querySelector('#convert-c-to-f').addEventListener('click', function() {
+    var fahrenheitVal, celsiusVal;
+
+    // grab the value inside the celsius box, convert it to a float.
+    celsiusVal = parseFloat(document.querySelector('#celsius').value, 10);
+
+    //calculate the correct value for fahrenheit, to 3 decimal places.
+    fahrenheitVal = (celsiusVal * (9.0 / 5.0) + 32).toFixed(3);
+
+    // catch cases where the user gave something that wasn't a number
+    if (isNaN(fahrenheitVal)) {
+      document.querySelector('#fahrenheit').value = 'Bad input.';
+    } else {
+      document.querySelector('#fahrenheit').value = fahrenheitVal; // put the value inside the text-box
+    }
+
+
+
+  }, false);
+
+
 
 }, false); //end main-listener
