d5e48c3434cb14bc5696c8fb77261a76b6d040ba
Chad Fry <chadefry@gmail.com>
2019-02-05 20:08:44 -0600

    Created Small example code for C/F conversion
    it isn't much, but this should give us a nice starting point for how to select things, and output others.

--- Studio04/project/script.js
+++ Studio04/project/script.js
@@ -2,17 +2,19 @@
 
 // CS 3312, spring 2019
 // Studio 4
-// YOUR NAMES:
+// YOUR NAMES: Chad Fry, Josh Meek
 
 // All the code below will be run once the page content finishes loading.
-document.addEventListener('DOMContentLoaded', function () {
-   'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  /* Example code given in-class!
 
    //Declare this function's local vars.
   var outputElement, num, whatever;
 
   // Save the output box in a var.
-  outputElement = document.querySelector('#output');
+  outputElement = document.querySelector('#output'); //we can use querySelector to get ANYTHING :O
 
   //Empty the text in the output box.
   outputElement.value = '';
@@ -30,4 +32,18 @@ document.addEventListener('DOMContentLoaded', function () {
       outputElement.value += num.toFixed(6) +  '\n';
   }
 
-}, false);
+
+*/
+
+
+  //Actual code goes down below.
+  document.querySelector('#convert-f-to-c').addEventListener('click', function() {
+    var fahrenheitVal, celsiusVal;
+
+    //grab the value inside the fahrenheit box, but convert it to an int (by default it may be string).
+    fahrenheitVal = parseInt(document.querySelector('#fahrenheit').textContent);
+    //test outputting before we get to any serious work.
+    document.querySelector('#celsius').value = 'The type of FV is: ' + typeof fahrenheitVal;
+  }, false); //end of
+
+}, false); //end main-listener
