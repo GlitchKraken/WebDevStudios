41bd982648e4954efa3b0d0eb55b447f01105e99
Josh <jmeek1997@yahoo.com>
2019-03-26 11:01:40 -0500

    Studio 8 Setup
    Studio 8 Setup

---
+++ Studio08/project/script.js
@@ -0,0 +1,45 @@
+/*jslint browser: true, indent: 3 */
+
+// CS 3312, spring 2019
+// Studio 8
+// YOUR NAMES: 
+
+// All the code below will be run once the page content finishes loading.
+document.addEventListener('DOMContentLoaded', function () {
+   'use strict';
+
+   (function () {
+      var createTextKeeper;
+
+      // WRITE YOUR createTextKeeper FUNCTION HERE
+
+      (function () {
+         var textKeeper, updateTextKeeper;
+
+         // WRITE YOUR updateTextKeeper FUNCTION HERE
+
+         // WRITE CODE FOR THE CONTROLLER HERE
+
+         // WRITE CODE TO GET THINGS STARTED HERE
+
+      }());
+   }());
+
+   (function () {
+      var createCounter;
+
+      // WRITE YOUR createCounter FUNCTION HERE
+
+      (function () {
+         var cardCounter, updateCards;
+
+         // WRITE YOUR updateCards FUNCTION HERE
+
+         // WRITE CODE FOR THE CONTROLLER HERE
+
+         // WRITE CODE TO GET THINGS STARTED HERE
+
+      }());
+   }());
+
+}, false);
