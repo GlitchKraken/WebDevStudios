9d2e72864e56f6e44620b9e45a6201785f094a03
Josh <jmeek1997@yahoo.com>
2019-04-09 11:33:02 -0500

    Names are there :D
    Names are there :D

--- Studio09/project/script.js
+++ Studio09/project/script.js
@@ -2,7 +2,7 @@
 
 // CS 3312, spring 2019
 // Studio 9
-// YOUR NAMES: 
+// YOUR NAMES: Josh Meek and Chad Fry
 
 // All the code below will be run once the page content finishes loading.
 document.addEventListener('DOMContentLoaded', function () {
