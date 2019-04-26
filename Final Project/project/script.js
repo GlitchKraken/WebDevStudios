431eae7baa9d881371b8382e045fdbc2aee360a5
SpiffyLich (Chad Fry) <chadefry@gmail.com>
2019-04-26 12:44:09 -0500

    Board setup should be mostly complete.
    the initial board-state / randomization should be finished.

--- Final Project/project/script.js
+++ Final Project/project/script.js
@@ -13,7 +13,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
    //Create a factory that makes an object to keep track of the WumpusWorld
    createWumpusWorld = function (oldState) {
-      var self, state, randomizeWumpusBoard;
+      var self, state, x,y, randomizeWumpusBoard;
+
+      randomizeWumpusBoard = function () {
+         //Randomize Wumpus Location.
+         do {
+            // set the x and y coordinates to random-numbers between 0 and 3
+            state.wumpusLocation.x = Math.floor(Math.random() * 4);
+            state.wumpusLocation.y = Math.floor(Math.random() * 4);
+            // make sure the wumpus doesn't start on top of the player.
+         } while(state.wumpusLocation.x === 0 && state.wumpusLocation.y === 0);
+
+
+         // Randomize Gold Location.
+         state.goldLocation.x = Math.floor(Math.random() * 4);
+         state.goldLocation.y = Math.floor(Math.random() * 4);
+
+         // Randomize Pits.
+         for(x = 0; x < 4; x++) {
+            for(y = 0; y < 4; y++) {
+               if(x === 0 && y === 0) {
+                  // always make sure spawn is not a pit.
+                  state.isPit[x][y] = false;
+               } else {
+                  // generate a random num between 0 and 4 inclusive (so each num has 20% chance)
+                  // here I pick 3 arbitrarily.
+                  // if we generate 3, then that pit gets enabled.
+                  if(Math.floor(Math.random() * 5) === 3) {
+                     isPit[x][y] = true;
+                  }
+               }
+            }
+         }
+
+      };
+
 
       //Create a default starting state.
       state = {
@@ -22,24 +56,50 @@ document.addEventListener('DOMContentLoaded', function () {
             y: 0
          },
          goldLocation: {
-            x: (Math.floor(Math.random() * (5 - 0)) + 0),
-            y: (Math.floor(Math.random() * (5 - 0)) + 0)
+            x: 0,
+            y: 0
          },
          wumpusLocation: {
-            x: (Math.floor(Math.random() * (5 - 1)) + 1),
-            y: (Math.floor(Math.random() * (5 - 1)) + 1)
+            // make sure the wumpus starts in an impossible place.
+            // we don't want to start with the player dying.
+            // he will get randomized when the randomizeWumpusBoard funciton
+            // is called.
+            x:  -10,
+            y:  -10
          },
+
+         // a 2d array of pits. by default, nothing is a pit.
+         // we can use this as follows: if (isPit[playerLocation.x][playerLocation.y]) {gameOver()};
+
+         // NOTE FOR JOSH: you may be concerened that 0,0 in the isPit array
+         // does not directly correspond to 0,0 on the wumpus-map (0,0 here is in top-left
+         // because of JavaScript, and not the bottom-left!). However, we will simply treat it
+         // like a parallel  array in CS 1.
+
+         // in other words, as long as we are consistant in our mapping of
+         // coordinates, there will be no need for translation.
+         isPit: [
+            [false, false, false, false],
+            [false, false, false, false],
+            [false, false, false, false],
+            [false, false, false, false]
+         ],
+
 
          playerMoves: 0,
          hasArrow: true,
-         pits: [],
-         perceptText: ''
-
-
+         perceptText: '',
+         playerScore: 0,
+         playerWin: false,
+         playerLose: false
       };
 
+      // shuffle everything up for the beginning of the round.
+      // (I looked at the example from studio-8, doing this only effects
+      // newgames. refreshes)
+      randomizeWumpusBoard();
       //Probably don't need this. Each cavern should have a default state
-      /*
+
       //If there is a valid previous state, use it instead.
       if (typeof oldState === 'string') {
          try {
@@ -47,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
          } catch (ignore) {
          }
       }
-      */
+
 
       //The self object contains public methods
       self = {
